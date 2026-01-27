4d1651916dc6c3edc400f1f32dbe7621da71b406
Kaue-Mota <pckaue123@gmail.com>
2026-01-27 15:56:50 -0300

    feat: update Header component styles for improved visibility and responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import "../../styles/global.css";
 
 export const Header = () => {
   return (
-    <header className="shadow-md flex z-100 text-white justify-between flex-row fixed bg-[rgba(71,133,31,0.1)] px-5 py-1 top-30 left-[50%] translate-[-50%] w-[70%] md:w-[60%] lg:w-[85%] max-w-8xl rounded-3xl backdrop-blur-md text-2xl items-center">
+    <header className="shadow-md flex z-100 justify-between flex-row fixed lg:bg-[rgba(115,182,88,0.2)]  bg-[#ffffff] px-5 py-1 top-20 lg:top-30 left-[50%] translate-[-50%] w-[70%] md:w-[60%] lg:w-[85%] max-w-8xl rounded-3xl backdrop-blur-lg text-2xl items-center">
       <a href="#">
         <img
           className="w-full h-10 md:h-20 object-cover drop-shadow-md scale-110"
@@ -17,7 +17,7 @@ export const Header = () => {
         id="menu-toggle"
       />
       <label htmlFor="menu-toggle" className="lg:hidden">
-        <img className="w-7 brightness-0 drop-shadow-md invert cursor-pointer" src="/images/icon/hamb.svg" alt="menu mobile" />
+        <img className="w-7 brightness-100 drop-shadow-md invert cursor-pointer" src="/images/icon/hamb.svg" alt="menu mobile" />
       </label>
 
       <nav className="lg:hidden bg-[rgba(255,255,255,1)] text-black rounded-2xl z-101 fixed top-22 right-[-130%] transition-right duration-300 items-start p-5 nav-slide backdrop-blur-lg  ">
@@ -45,7 +45,7 @@ export const Header = () => {
         </ul>
       </nav>
 
-      <nav className=" text-white hidden lg:inline-block">
+      <nav className=" md:text-white text-black hidden lg:inline-block">
         <ul className="flex text-center justify-between gap-1 drop-shadow-md">
           <li className="p-2 hover:text-green-400 transition-colors duration-200 cursor-pointer">
             <a href="" className="underline-expand">
